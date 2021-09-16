44cb06a985e223ceee1aff0be1643d90943be17e
Sido Haakma <sido@haakma.org>
2021-09-16 10:18:38 +0200

    chore: fix setting authentication for docker hub

--- .azure/build_docker.bash
+++ .azure/build_docker.bash
@@ -12,7 +12,7 @@
 echo "Set the docker authentication configuration"
 DOCKER_CONFIG=${AGENT_HOMEDIRECTORY}/.docker
 mkdir -p ${DOCKER_CONFIG}
-set +x && echo "{\"auths\": \"https://index.docker.io/v1/\": {\"auth\": \"${DOCKERHUB_AUTH}\"}, \"registry.hub.docker.com\": {\"auth\": \"${DOCKERHUB_AUTH}\"}}}' > ${DOCKER_CONFIG}/config.json"
+set +x && echo "{\"auths\": \"https://index.docker.io/v1/\": {\"auth\": \"${DOCKERHUB_AUTH}\"}, \"registry.hub.docker.com\": {\"auth\": \"${DOCKERHUB_AUTH}\"}}}" > "${DOCKER_CONFIG}/config.json"
 
 
 envs=($(ls -d */))
