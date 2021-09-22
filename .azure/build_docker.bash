63e451989fcc8906c154c888e1e0a5b92a7f4260
Sido Haakma <sido@haakma.org>
2021-09-22 19:33:07 +0200

    fix: set docker config in current dit

--- .azure/build_docker.bash
+++ .azure/build_docker.bash
@@ -9,10 +9,8 @@
 # - DOCKERHUB_AUTH: we use this credential to push the dockers to the registry
 # - GITHUB_TOKEN: semantic release uses this environment variable to push to github
 
-echo "Set the docker authentication configuration in ~/.docker"
-DOCKER_CONFIG="~/.docker"
-mkdir -p "${DOCKER_CONFIG}"
-set +x && echo "{\"auths\": {\"https://index.docker.io/v1/\": {\"auth\": \"${DOCKERHUB_AUTH}\"}, \"registry.hub.docker.com\": {\"auth\": \"${DOCKERHUB_AUTH}\"}}}" > "${DOCKER_CONFIG}/config.json"
+echo "Set the docker authentication configuration in ${pwd}/.docker"
+set +x && echo "{\"auths\": {\"https://index.docker.io/v1/\": {\"auth\": \"${DOCKERHUB_AUTH}\"}, \"registry.hub.docker.com\": {\"auth\": \"${DOCKERHUB_AUTH}\"}}}" > "${pwd}/config.json"
 
 cd "${BUILD_REPOSITORY_LOCALPATH}"
 envs=($(ls -d */))
